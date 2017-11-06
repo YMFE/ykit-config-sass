726d781a438ff390eebee5a329816e3f10bf0847
yuhao.ju <yuhao.ju@qunar.com>
2017-11-06 16:01:12 +0800

    feat: add extract option

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 
 module.exports = {
-    config: function(plugins) {
+    config: function(options) {
         var self = this;
+        var extractOption = options.extract || 'always';
 
         return {
             // 更改 Webpack 配置
             modifyWebpackConfig: function(baseConfig) {
                 var isExists = false;
+
                 var sassLoaderObj = {
                     test: /\.(sass|scss)$/,
                     loader: hadnleExtractLoaders([
@@ -39,16 +41,25 @@ module.exports = {
 
         function hadnleExtractLoaders (loaders) {
             var loaderString = loaders.map(function(loader) {
-                return require.resolve(loader) + (self.env === 'prd' ? '!' : '?sourceMap!')
-            }).join('');
+                return require.resolve(loader) + (self.env === 'prd' ? '' : '?sourceMap')
+            }).join('!');
 
-            return self.env === 'local'
-            ? require.resolve('style-loader') + '!' + loaderString
-            : plugins.ExtractTextPlugin.extract(
+            var extract = options.ExtractTextPlugin.extract(
                 self.webpack.version && self.webpack.version >= 2
                 ? require.resolve('style-loader') + '!' + loaderString
                 : require.resolve('style-loader'), loaderString
             )
+
+            var noExtract = require.resolve('style-loader') + '!' + loaderString;
+
+            switch (extractOption) {
+                case 'auto':
+                    return self.env === 'local' ? noExtract : extract;
+                case 'always':
+                    return extract;
+                case 'never':
+                    return noExtract;
+            }
         }
     }
 }
